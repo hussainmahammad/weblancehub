2b60ff2bda4c47437ec02c3080594d3907676f81
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-25 10:24:53 +0530

    Update backend.tf

--- files/ec2/terraform/backend.tf
+++ files/ec2/terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "weblancehub-terraform-state-867809929056"
-    key            = "weblancehub/frontend/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "weblancehub-terraform-locks"
-    encrypt        = true
+    bucket  = "hussainmahammad.online-tfstates"
+    key     = "weblancehub/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
